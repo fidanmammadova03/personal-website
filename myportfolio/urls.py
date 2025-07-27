565466dcb9ba54223c027a854eb27e803abc3e71
fidanmammadova03 <fidan.mammadova03@icloud.com>
2025-07-27 15:08:34 +0400

    fix

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
