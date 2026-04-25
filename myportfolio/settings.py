18ab981411ceb3f16ae7f7ed6e7f9f5835ae6815
fidanmammadova03 <fidan.mammadova03@icloud.com>
2026-04-26 02:42:13 +0400

    final

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -47,6 +47,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
+# Security settings for HTTPS (Render serves over HTTPS)
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
